04310011320d3942847c95ef99c5ac0c6e517777
Alexis Fellenius Makrigianni <work@lexi.se>
2021-07-05 15:58:25 +0200

    Animate selected elements on scroll

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -5,3 +5,44 @@ const application = Application.start();
 const context = require.context('./controllers', true, /\.js$/);
 
 application.load(definitionsFromContext(context));
+
+// Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.AnimateOnScroll');
+
+function loop() {
+
+    Array.prototype.forEach.call(elementsToShow, function(element){
+      if (isElementInViewport(element)) {
+        element.classList.add('AnimateOnScroll--isVisible');
+      }
+    });
+
+    scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  console.log(document.documentElement.clientHeight);
+
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
